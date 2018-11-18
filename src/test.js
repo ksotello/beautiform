17217252c6385f3259e4f461d123b231588e2a15
Kyle Sotello <opticalvoid@gmail.com>
2018-11-17 19:33:01 -0500

    Adding enzyme

--- src/test.js
+++ src/test.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import '../setupTests'
+
 import ExampleComponent from './'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
     expect(ExampleComponent).toBeTruthy()
+  })
+
+  it('should mount', done => {
+    const wrapper = mount(<ExampleComponent />)
+    expect(wrapper.exists()).toBe(true)
+    wrapper.unmount()
+    done()
   })
 })
